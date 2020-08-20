ed7d0a20e12f0141c51f8bf22c91aa959a8d79a0
Evan Chisholm <chisho21@gmail.com>
2020-08-20 07:39:18 -0400

    BasketService pt 2, added interface basket service

---
+++ MyShop/MyShop.Core/ViewModels/BasketSummaryViewModel.cs
@@ -0,0 +1,24 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.ViewModels
+{
+    public class BasketSummaryViewModel
+    {
+        public int BasketCount { get; set; }
+        public decimal BasketTotal { get; set; }
+        public BasketSummaryViewModel()
+        {
+            //empty on purpose
+        }
+        public BasketSummaryViewModel(int basketCount, decimal basketTotal)
+        {
+            this.BasketCount = basketCount;
+            this.BasketTotal = basketTotal;
+        }
+    }
+}
